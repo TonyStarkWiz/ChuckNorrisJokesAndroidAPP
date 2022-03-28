eebaa02b2be4effc0b9eeed14127d4651270b9cb
TonyStarkWiz <tony_stark@knights.ucf.edu>
2022-03-28 12:39:24 -0400

    REST BASE/PATH/QUERY

---
+++ app/src/main/java/com/android/chucknorrisjokesandroidapp/rest/Services.kt
@@ -0,0 +1,25 @@
+package com.android.chucknorrisjokesandroidapp.rest
+
+import com.android.chucknorrisjokesandroidapp.model.JokesResponse
+import com.android.chucknorrisjokesandroidapp.model.Value
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+
+interface Services {
+    @GET(pathOfAPI)
+    suspend fun getJokes(
+        @Path("pathOfAPI") jokesID : String,
+        @Query("firstName") firstName: String,
+        @Query("lastName") lastName: String
+    ): Response<JokesResponse>
+
+    companion object {
+        //http://api.icndb.com/jokes/random?firstName=Chuck&lastName=Norris
+        const val baseURL = "http://api.icndb.com/jokes/"
+        private const val pathOfAPI = "random"
+
+    }
+}
