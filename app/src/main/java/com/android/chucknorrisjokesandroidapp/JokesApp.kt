d01b65e8a2047d310885cc8ff6069b68eb81d119
TonyStarkWiz <tony_stark@knights.ucf.edu>
2022-04-02 17:59:08 -0400

    (n+4)commit

--- app/src/main/java/com/android/chucknorrisjokesandroidapp/JokesApp.kt
+++ app/src/main/java/com/android/chucknorrisjokesandroidapp/JokesApp.kt
@@ -1,9 +1,42 @@
 package com.android.chucknorrisjokesandroidapp
 
 import android.app.Application
+import com.android.chucknorrisjokesandroidapp.di.ApplicationModule
+import com.android.chucknorrisjokesandroidapp.di.DaggerJokesComponent
+import com.android.chucknorrisjokesandroidapp.di.JokesComponent
 
 class JokesApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        jokesComponent = DaggerJokesComponent
+            .builder()
+            .applicationModule(ApplicationModule(this))
+            .build()
+
+    }
+    companion object{
+        lateinit var jokesComponent: JokesComponent
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
